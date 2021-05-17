d96f3ebb523cd33aeddf67ed566eda3570c9bc78
Ddudooo <igoa.work@gmail.com>
2021-05-17 22:07:43 +0900

    기본 문법
    JPQL 과 QueryDSL 문법 비교

---
+++ src/test/kotlin/study/kotlin/querydsl/QueryDslBasicTest.kt
@@ -0,0 +1,64 @@
+package study.kotlin.querydsl
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
+import study.kotlin.querydsl.entity.Member
+import study.kotlin.querydsl.entity.QMember
+import study.kotlin.querydsl.entity.Team
+import javax.persistence.EntityManager
+
+@SpringBootTest
+@Transactional
+class QueryDslBasicTest(
+    @Autowired
+    private val em : EntityManager
+) {
+
+    @BeforeEach
+    fun before() {
+        val teamA = Team("teamA")
+        val teamB = Team("teamB")
+        em.persist(teamA)
+        em.persist(teamB)
+
+        val member1 = Member("member1", 10, teamA)
+        val member2 = Member("member2", 20, teamA)
+
+        val member3 = Member("member3", 30, teamB)
+        val member4 = Member("member4", 40, teamB)
+        em.persist(member1)
+        em.persist(member2)
+        em.persist(member3)
+        em.persist(member4)
+    }
+
+    @Test
+    fun startJPQL(){
+        //member1찾기
+        val findMember1 = em.createQuery(
+            "select m from Member m where m.username = :username",
+            Member::class.java
+        )
+            .setParameter("username", "member1")
+            .singleResult
+
+        assertThat(findMember1.username).isEqualTo("member1")
+    }
+
+    @Test
+    fun startQueryDsl(){
+        val jpaQueryFactory = JPAQueryFactory(em)
+        val m = QMember("m")
+        val fetchOne = jpaQueryFactory.select(m)
+            .from(m)
+            .where(m.username.eq("member1"))
+            .fetchOne()
+
+        assertThat(fetchOne?.username).isEqualTo("member1")
+    }
+}
